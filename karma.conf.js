71e7d508f61cbfe64038cb4c2361ad7983cc8709
andela-uofoegbu <ugonna.ofoegbu@andela.com>
2017-01-30 15:00:16 +0100

    update eslintrc

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'src/inverted-index.js',
       'jasmine/build/bundle.js'
     ],
 
